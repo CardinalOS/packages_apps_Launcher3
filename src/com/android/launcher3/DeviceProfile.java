b307f3a25de78bf02679461b96dba2ab15eefae4
Dhina17 <dhinalogu@gmail.com>
2023-09-09 11:07:12 +0530

    Launcher3: Fix hotseat border space properly
    It fixes hotseat icons visibility in landscape.
    
    Revert "Launcher3: Remove scalable grid check for calculating QSB width"
    
    This reverts commit 5e22e56459c01cac7a8e141b3e421aa429745f69.
    
    Revert "Launcher3: Calculate hotseat border space only if QSB available"
    
    This reverts commit 2b0feef084072b61b204c2b562ff57d7804ad3fd.
    
    Change-Id: Iec032dffbe01f05f60e26a69c9d3982065102736

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -219,7 +219,6 @@ public class DeviceProfile {
     private final int mMinHotseatQsbWidthPx;
     private final int mMaxHotseatIconSpacePx;
     public final int inlineNavButtonsEndSpacingPx;
-    private boolean mShowQsb;
 
     // Bottom sheets
     public int bottomSheetTopPadding;
@@ -492,12 +491,12 @@ public class DeviceProfile {
         hotseatQsbVisualHeight = hotseatQsbHeight - 2 * hotseatQsbShadowHeight;
 
         // Whether QSB might be inline in appropriate orientation (e.g. landscape).
-        mShowQsb = Utilities.showQSB(context);
-        boolean canQsbInline = mShowQsb && (isTwoPanels ? inv.inlineQsb[INDEX_TWO_PANEL_PORTRAIT]
+        boolean showQsb = Utilities.showQSB(context);
+        boolean canQsbInline = showQsb && (isTwoPanels ? inv.inlineQsb[INDEX_TWO_PANEL_PORTRAIT]
                 || inv.inlineQsb[INDEX_TWO_PANEL_LANDSCAPE]
                 : inv.inlineQsb[INDEX_DEFAULT] || inv.inlineQsb[INDEX_LANDSCAPE])
-                && hotseatQsbHeight > 0 && Utilities.showQSB(context);
-        isQsbInline = inv.inlineQsb[mTypeIndex] && canQsbInline;
+                && hotseatQsbHeight > 0;
+        isQsbInline = isScalableGrid && inv.inlineQsb[mTypeIndex] && canQsbInline;
 
         areNavButtonsInline = isTaskbarPresent && !isGestureMode;
         numShownHotseatIcons =
@@ -506,7 +505,7 @@ public class DeviceProfile {
         numShownAllAppsColumns =
                 isTwoPanels ? inv.numDatabaseAllAppsColumns : inv.numAllAppsColumns;
 
-        int hotseatBarBottomSpace = mShowQsb || isTaskbarPresent ? pxFromDp(
+        int hotseatBarBottomSpace = showQsb || isTaskbarPresent ? pxFromDp(
                 inv.hotseatBarBottomSpace[mTypeIndex], mMetrics) : 0;
         int minQsbMargin = res.getDimensionPixelSize(R.dimen.min_qsb_margin);
 
@@ -515,13 +514,13 @@ public class DeviceProfile {
                     HotseatSpecs.create(new ResourceHelper(context,
                             isTwoPanels ? inv.hotseatSpecsTwoPanelId : inv.hotseatSpecsId));
             mResponsiveHotseatSpec = hotseatSpecs.getCalculatedHeightSpec(heightPx);
-            hotseatQsbSpace = mShowQsb ? mResponsiveHotseatSpec.getHotseatQsbSpace() : 0;
+            hotseatQsbSpace = ShowQsb ? mResponsiveHotseatSpec.getHotseatQsbSpace() : 0;
         } else {
-            hotseatQsbSpace = mShowQsb ? pxFromDp(inv.hotseatQsbSpace[mTypeIndex], mMetrics) : 0;
+            hotseatQsbSpace = ShowQsb ? pxFromDp(inv.hotseatQsbSpace[mTypeIndex], mMetrics) : 0;
         }
 
         // Have a little space between the inset and the QSB
-        if (mShowQsb && mInsets.bottom + minQsbMargin > hotseatBarBottomSpace) {
+        if (showQsb && mInsets.bottom + minQsbMargin > hotseatBarBottomSpace) {
             int availableSpace = hotseatQsbSpace - (mInsets.bottom - hotseatBarBottomSpace);
 
             // Only change the spaces if there is space
@@ -767,6 +766,8 @@ public class DeviceProfile {
      * necessary.
      */
     public void recalculateHotseatWidthAndBorderSpace() {
+        if (!(isScalableGrid || (isTaskbarPresent && !isLandscape))) return;
+
         int columns = inv.hotseatColumnSpan[mTypeIndex];
         float hotseatWidthPx = getIconToIconWidthForColumns(columns);
         hotseatBorderSpace = calculateHotseatBorderSpace(hotseatWidthPx, /* numExtraBorder= */ 0);
@@ -1145,9 +1146,6 @@ public class DeviceProfile {
     private int calculateHotseatBorderSpace(float hotseatWidthPx, int numExtraBorder) {
         int numBorders = (numShownHotseatIcons - 1 + numExtraBorder);
         if (numBorders <= 0) return 0;
-
-        // Calculate hotseat border space only if QSB available.
-        if(!mShowQsb) return 0;
 
         float hotseatIconsTotalPx = iconSizePx * numShownHotseatIcons;
         int hotseatBorderSpacePx = (int) (hotseatWidthPx - hotseatIconsTotalPx) / numBorders;
